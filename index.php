428132d51eb7dc078bc88295d51395a0c2e73ca0
PalindroneV2 <asfadasfag@gmail.com>
2022-11-04 03:10:00 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,3 @@
-<?php
-
-require "db.php";
-
-session_start();
-
-if ($_POST){
-
-    if (strlen($_POST['username']) >= 1 && strlen($_POST['password']) >= 1 ){
-        $username = trim($_POST['username']);
-        $password = trim($_POST['password']);
-        $consulta = "SELECT * FROM cat_usuarios WHERE username ='$username' AND password = '$password'";
-        $resultado = $conex->query($consulta);
-		$num = $resultado->num_rows;
-        $mostrar = mysqli_fetch_array($resultado);
-
-        $id_u = $mostrar['id'];
-
-        if ($num>0){
-            $row = $resultado->fetch_assoc();
-			$password_bd = $row['password'];
-
-            if($password_bd == $pass_c){
-				
-				$_SESSION['id'] = $mostrar['id'];
-                $_SESSION['username'] = $mostrar['username'];
-				
-                $consulta = "INSERT INTO bitacora_acceso(id_usuario) 
-                    VALUES ('$id_u', now())";
-                $resultado = mysqli_query($conex,$consulta);
-            
-				header ("location:Menu/");
-				
-			} else {
-                header ("location:index.php?error=El usuario y/o contraseña no validos.");
-            }
-        } else {
-            header ("location:index.php?error=El usuario y/o contraseña no validos.");
-        }
-    } else {
-        header ("location:index.php?error=Introduzca su usuario y/o contraseña.");
-    }
-}
-
-
-?>
-
 <!DOCTYPE html>
 <html>
 
@@ -88,25 +41,9 @@ if ($_POST){
                     <div class="card mb-5">
                         <div class="card-body d-flex flex-column align-items-center">
                             <div class="bs-icon-xl bs-icon-circle bs-icon-primary bs-icon my-4" style="background: rgba(13,110,253,0);"><img src="assets/img/logoHeader.png"></div>
-                            <?php if(isset($_GET['error'])){ ?>
-                            <div id="error" style="width: 100%; background: lightsalmon; text-align: center; border-radius: 2px; padding: 4px; ">
-                                <label style="color: whitesmoke;"><?php echo $_GET['error']; ?></label>
-                                <span class="close" style="font-size: 24px; color: whitesmoke; margin: auto;" onclick="getElementById('error').style.display = 'none' ">&times;</span>
-                            </div>
-                            <?php } ?>
-                            <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" class="text-center">
+                            <form method="POST" action="../../Menu/index.php" class="text-center">
                                 <div class="mb-3">
-                                    <label class="form-label" for="username" required>Usuario:</label>
-                                    <input class="form-control" type="text" id="username" name="username"
-                                        maxlength="10" onkeypress="return ((event.charCode >= 65 && event.charCode <= 90) || (event.charCode >= 97 && event.charCode <= 122))">
-                                </div>
-                                <div class="mb-3">
-                                    <label class="form-label" for="password" required>Contraseña:<br></label>
-                                    <input class="form-control" type="password" id="password" name="password" 
-                                        maxlength="8" onkeypress="return (event.charCode >= 48 && event.charCode <= 57)">
-                                </div>
-                                <div class="mb-3">
-                                    <input class="btn btn-primary d-block w-100" type="submit" id="entrar" name="entrar" value="Entrar" onsubmit="return ValidarVacio()">
+                                    <input class="btn btn-primary d-block w-100" type="submit" value="Entrar">
                                 </div>
                                 <p class="text-muted"></p>
                             </form>
