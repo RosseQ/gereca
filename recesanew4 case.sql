fcd67e1d362efee85316217b0e382c0b10d5b515
PalindroneV2 <asfadasfag@gmail.com>
2023-01-02 19:29:35 -0700

    Update recesanew4 case.sql

--- recesanew4 case.sql
+++ recesanew4 case.sql
@@ -2,30 +2,29 @@ INSERT INTO detalle_renta (id_Vehiculo,cantidad) VALUES (5, 25);
 
 SELECT id_detalleRenta INTO @rent_det from detalle_renta order by id_detalleRenta DESC limit 1;
 
-SELECT 2 INTO @tiporent;
-SELECT 5 INTO @idveh_price_choose;
+SET @tiporent = 2;
+SET @idveh_price_choose = 5;
 
-
-IF @tiporent = 1 THEN
+CASE @tiporent
+    WHEN 1 THEN
     SELECT costos.precio_dia INTO @cost_prec from costos
     INNER JOIN vehiculos on costos.id_costo = vehiculos.economico
     WHERE costos.id_costo = vehiculos.economico
     AND Vehiculos.id_Vehiculo = @idveh_price_choose
     limit 1;
-END;
-IF @tiporent = 2 THEN
+    WHEN 2 THEN
     SELECT costos.precio_sem INTO @cost_prec from costos
     INNER JOIN vehiculos on costos.id_costo = vehiculos.economico
     WHERE costos.id_costo = vehiculos.economico
     AND Vehiculos.id_Vehiculo = @idveh_price_choose
     limit 1;
-END;
-IF @tiporent = 3 THEN
+    WHEN 3 THEN
     SELECT costos.precio_mes INTO @cost_prec from costos
     INNER JOIN vehiculos on costos.id_costo = vehiculos.economico
     WHERE costos.id_costo = vehiculos.economico
     AND Vehiculos.id_Vehiculo = @idveh_price_choose
     limit 1;
+    ELSE @cost_prec = 0
 END;
 
 INSERT INTO renta (id_cliente,id_detalleRenta,total,fecha)
