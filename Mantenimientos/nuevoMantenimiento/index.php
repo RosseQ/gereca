0a58d3fe309b9628c9db94f1fc0896ffe5f5b7fa
PalindroneV2 <asfadasfag@gmail.com>
2023-01-03 22:34:43 -0700

    Update index.php

--- Mantenimientos/nuevoMantenimiento/index.php
+++ Mantenimientos/nuevoMantenimiento/index.php
@@ -127,7 +127,8 @@ include("../../db.php");
                     <select class="form-select" id="idVehiculo_det" name="idVehiculo_det">
                         <?php 
                             $consulta = "SELECT id_Vehiculo, tipo_unidad
-                            FROM vehiculos";
+                            FROM vehiculos WHERE Vehiculos.id_VEstatus = 1
+                            AND Vehiculos.id_DEstatus = 1;";
                             $resultado = mysqli_query($conex,$consulta);
                             while($mostrar = mysqli_fetch_array($resultado)){
                         ?>
